ca109461b817e97ae1b1f4158da14cb79b788580
unknown <2098325@labinfo.is.escuelaing.edu.co>
2019-02-05 12:01:17 -0500

    Falta arreglar imagen

--- src/ServidorWeb/HttpServer.java
+++ src/ServidorWeb/HttpServer.java
@@ -3,18 +3,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejercicoi451;
+package ServidorWeb;
 
+import ejercicoi451.*;
 import ejercicio44.*;
+import java.awt.image.BufferedImage;
 import java.net.*;
 import java.io.*;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
+import javax.imageio.ImageIO;
+import sun.misc.IOUtils;
 
 public class HttpServer {
 
     public static void main(String[] args) throws IOException {
-
+        String tmpp="";
+        String pagina;
         
         List<ServerSocket> listServers = new ArrayList<ServerSocket>();
         int tmp = 0;
@@ -39,15 +45,50 @@ public class HttpServer {
                 System.exit(1);
             }
 
+            
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(
                     new InputStreamReader(
                             clientSocket.getInputStream()
                     ));
             String inputLine, outputLine;
-
+            BufferedImage imagen=null;
             while ((inputLine = in.readLine()) != null) {
                 System.out.println("Received: " + inputLine);
+                
+                try{
+                    System.out.println("Miremos in : "+inputLine);
+                    FileReader f = new FileReader("src/archivos/pagSW.html");
+
+                    //File ima=new File("src/archivos/bug.png");
+                    ///imagen=ImageIO.read(ima);
+                    
+                    //ByteArrayOutputStream output = new ByteArrayOutputStream();
+                    //byte[] buffer                = new byte[ 1024 ];
+                    //FileInputStream myStream = new FileInputStream("src/archivos/bug.png");
+
+                    //BufferedImage image = ImageIO.read(new File("src/archivos/bug.png")); 
+                    //ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(); 
+                    //ImageIO.write(image, "png", byteArrayOutputStream); 
+                    //byte[] size = ByteBuffer.allocate(4).putInt(byteArrayOutputStream.size()).array(); 
+
+                    
+                    BufferedReader b = new BufferedReader(f);
+                    while((pagina = b.readLine())!=null) {
+                        System.out.println(pagina);
+                        tmpp+=pagina;
+                    }
+                
+                }catch(Exception e){
+                
+                    System.err.print("No se encontro pagina");
+                    break;
+                }
+                
+                //src/archivos/pagSW.html
+                
+                
+                
                 if (!in.ready()) {
                     break;
                 }
@@ -55,16 +96,7 @@ public class HttpServer {
             outputLine = "HTTP/1.1 200 OK\r\n"
                     + "Content-Type: text/html\r\n"
                     + "\r\n"
-                    + "<!DOCTYPE html>\n"
-                    + "<html>"
-                    + "<head>"
-                    + "<meta charset=\"UTF-8\">"
-                    + "<title>Title of the document</title>\n"
-                    + "</head>"
-                    + "<body>"
-                    + "My Web Site"
-                    + "</body>"
-                    + "</html>" + inputLine;
+                    +tmpp ;
             out.println(outputLine);
 
             out.close();
